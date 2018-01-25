d087f6d48bd667fda363c41066aae5e1f34c72c7
Zdenek Pribyla <zdenek.pribyla@gmail.com>
2018-01-25 13:43:50 +0100

    add working script for loading ties

--- ties-navbar.js
+++ ties-navbar.js
@@ -1,18 +1,18 @@
+document.addEventListener('DOMContentLoaded', function () {
 
-document.addEventListener('DOMContentLoaded', function() {
+    var tiesData = [
+        {name: 'red', imgSrc: 'img/tie-red.png', link: 'https://vasekravaty.cz/cervene-kravaty'},
+        {name: 'green', imgSrc: 'img/tie-green.png', link: 'https://vasekravaty.cz/cervene-kravaty'},
+        {name: 'blue', imgSrc: 'img/tie-blue.png', link: 'https://vasekravaty.cz/cervene-kravaty'},
+        {name: 'purple', imgSrc: 'img/tie-purple.png', link: 'https://vasekravaty.cz/cervene-kravaty'}
+    ];
 
+    var tieElementTarget = document.getElementById('ties-navbar');
 
-
-var getTies = function(){
-
-    var element = document.createElement("div");
-    var text = document.createTextNode("Here are my Ties");
-    element.appendChild(text);
-
-    return document.getElementById('ties').appendChild(element);
-
-}
-window.onload=getTies();
-
+    tiesData.forEach(function (tie) {
+            var tieElement = ('<div class="tie__block"><a href="' + tie.link + '"><img class="tie__icon" src="' + tie.imgSrc + '"></a></div>');
+            tieElementTarget.insertAdjacentHTML('beforeend', tieElement);
+        }
+    );
 
 }, false);
