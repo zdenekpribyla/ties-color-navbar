bd626ce6c7d6b4a3488affb0e0a6a1b1f80ee634
Zdenek Pribyla <zdenek.pribyla@gmail.com>
2018-01-30 17:35:30 +0100

    WIP: cover all iterated object to one element

--- ties-navbar.js
+++ ties-navbar.js
@@ -6,13 +6,13 @@ document.addEventListener('DOMContentLoaded', function () {
         {name: 'blue', imgSrc: 'img/tie-blue.png', link: 'https://vasekravaty.cz/cervene-kravaty'},
         {name: 'purple', imgSrc: 'img/tie-purple.png', link: 'https://vasekravaty.cz/cervene-kravaty'}
     ];
-
     var tieElementTarget = document.getElementById('ties-navbar');
+
 
     tiesData.forEach(function (tie) {
             var tieElement = ('<div class="tie__block"><a href="' + tie.link + '"><img class="tie__icon" src="' + tie.imgSrc + '"></a></div>');
+
             tieElementTarget.insertAdjacentHTML('beforeend', tieElement);
-        }
-    );
+        });
 
 }, false);
