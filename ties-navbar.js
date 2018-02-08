a6656cec1cbc09f092a4dfee9e7e74574be04a9b
Zdenek Pribyla <zdenek.pribyla@gmail.com>
2018-02-08 10:49:55 +0100

    improve demo ties navbar

--- ties-navbar.js
+++ ties-navbar.js
@@ -6,34 +6,55 @@ document.addEventListener('DOMContentLoaded', function () {
         {name: 'blue', imgSrc: 'img/tie-blue.png', link: 'https://vasekravaty.cz/cervene-kravaty'},
         {name: 'purple', imgSrc: 'img/tie-purple.png', link: 'https://vasekravaty.cz/cervene-kravaty'}
     ];
-    var tieElementTarget = document.getElementById('ties-navbar');
 
-
+    var tieElementTarget = document.getElementById('ties-container');
+    var tieElements = [];
     tiesData.forEach(function (tie) {
         var tieElement = ('<div class="tie__block"><a href="' + tie.link + '"><img class="tie__icon" src="' + tie.imgSrc + '"></a></div>');
-
-        tieElementTarget.insertAdjacentHTML('beforeend', tieElement);
+        tieElements.push(tieElement);
     });
 
-    var priceNavbarTarget = document.getElementById('price__navbar')
-    var priceElement = (
-        '<div class="price__benefit">\n' +
-        '        <div class="price__benefit-single">\n' +
-        '            <div class="price__benefit-single__icon"><img src="http://pribyla.com/images/vesak.png"\n' +
-        '                                                          class="price__benefit-single__img"></div>\n' +
-        '            <div class="price__benefit-single__text"><span>dffffff  ve fólii s malým věšáčkem</span></div>\n' +
-        '        </div>\n' +
-        '        <div class="price__benefit-single">\n' +
-        '            <div class="price__benefit-single__icon"><img src="http://pribyla.com/images/vesak.png"\n' +
-        '                                                          class="price__benefit-single__img"></div>\n' +
-        '            <div class="price__benefit-single__text"> dffffff ve fólii s malým věšáčkem</div>\n' +
-        '        </div>\n' +
-        '        <div class="price__benefit-single">\n' +
-        '            <div class="price__benefit-single__icon"><img src="http://pribyla.com/images/vesak.png"\n' +
-        '                                                          class="price__benefit-single__img"></div>\n' +
-        '            <div class="price__benefit-single__text"><span>dffffff  ve fólii s malým věšáčkem</span></div>\n' +
-        '        </div>\n' +
-        '    </div>'
-    )
-    priceNavbarTarget.insertAdjacentHTML('beforeend', priceElement)
-}, false);
+
+    function getSum(total, num) {
+        return total + num;
+    }
+    function tieContainer() {
+         return '<div class="ties__navbar">' + tieElements.reduce(getSum) + '</div>';
+
+    }
+    tieElementTarget.insertAdjacentHTML('beforeend', tieContainer())
+
+
+        // var tieElementTarget = document.getElementById('ties-navbar');
+        //
+        // tiesData.forEach(function (tie) {
+        //     var tieElement = ('<div class="tie__block"><a href="' + tie.link + '"><img class="tie__icon" src="' + tie.imgSrc + '"></a></div>');
+        //
+        //     tieElementTarget.insertAdjacentHTML('beforeend', tieElement);
+        // });
+
+
+        // var priceNavbarTarget = document.getElementById('price__navbar')
+        // var priceElement = (
+        //     '<div class="price__benefit">\n' +
+        //     '        <div class="price__benefit-single">\n' +
+        //     '            <div class="price__benefit-single__icon"><img src="http://pribyla.com/images/vesak.png"\n' +
+        //     '                                                          class="price__benefit-single__img"></div>\n' +
+        //     '            <div class="price__benefit-single__text"><span>dffffff  ve fólii s malým věšáčkem</span></div>\n' +
+        //     '        </div>\n' +
+        //     '        <div class="price__benefit-single">\n' +
+        //     '            <div class="price__benefit-single__icon"><img src="http://pribyla.com/images/vesak.png"\n' +
+        //     '                                                          class="price__benefit-single__img"></div>\n' +
+        //     '            <div class="price__benefit-single__text"> dffffff ve fólii s malým věšáčkem</div>\n' +
+        //     '        </div>\n' +
+        //     '        <div class="price__benefit-single">\n' +
+        //     '            <div class="price__benefit-single__icon"><img src="http://pribyla.com/images/vesak.png"\n' +
+        //     '                                                          class="price__benefit-single__img"></div>\n' +
+        //     '            <div class="price__benefit-single__text"><span>dffffff  ve fólii s malým věšáčkem</span></div>\n' +
+        //     '        </div>\n' +
+        //     '    </div>'
+        // )
+        // priceNavbarTarget.insertAdjacentHTML('beforeend', priceElement)
+
+
+    }, false);
